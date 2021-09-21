e923f03bdf6fdd9ddbadebda0a79054a0241f8c6
ChristianFarnsworth23 <834243@mystma.org>
2021-09-21 14:50:16 -0500

    3

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a  + b  + c  + d  + e 
+
+print(topFiveMovies("The Suicide Squad ", "Luca ", "Shrek ", "Free Guy ", "The Greatest Showman"))
+
+
