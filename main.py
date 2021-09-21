e6c67f1da90e6c059e624cc6601ab10ef16c112a
ChristianFarnsworth23 <834243@mystma.org>
2021-09-21 14:57:38 -0500

    3

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 def topFiveMovies(a , b , c , d , e ):
-    return a + b + c + d + e 
+    return a + " " + b + " " + c + " " + d + " "+ e 
 
-print(topFiveMovies("The Suicide Squad ", "Luca ", "Shrek ", "Free Guy ", "The Greatest Showman"))
-
-
+print(topFiveMovies("The Suicide Squad",                          "Luca", "Shrek", "Free Guy", "The Greatest Showman"))
